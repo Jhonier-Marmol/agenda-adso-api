1e2dd7712546a277d7187467a3de6ab701f1d046
Jhonier Alexander Marmol Montero <Jhoniermarmol92@gmail.com>
2026-09-08 15:01:28 -0500

    Configure server for Render

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
 const PORT = process.env.PORT || 3000;
+
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
 server.use(router);
-server.listen(PORT, () => {
-    console.log(`JSON Server está corriendo en el puerto ${PORT}`);
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`JSON Server funcionando en el puerto ${PORT}`);
 });
